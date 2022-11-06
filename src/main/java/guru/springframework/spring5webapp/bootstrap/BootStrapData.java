bf14f1d47411cb3d2985eddb922dc7e30a22744b
Neil Goldsmith <neilgoldsmith1218@gmail.com>
2022-11-05 21:56:23 -0400

    publisher, author, books

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("J2EE Develoment without EJB","1232132132");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        Publisher pub = new Publisher("John","100 elms st","Orlando","FL","32828");
+        Publisher pub1 = new Publisher("John1","100 elms st","Orlando","FL","32828");
+        publisherRepository.save(pub);
+        publisherRepository.save(pub1);
+
+
+        System.out.println("started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Pubs: " + publisherRepository.count());
+
+        for(Publisher p:(List<Publisher>) publisherRepository.findAll()){
+            System.out.println("pub ID:" + p.getId());
+        }
+    }
+}
